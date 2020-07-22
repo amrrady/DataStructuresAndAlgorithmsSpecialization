a61f89137302dd2e11b9b2ec839e38d87719de74
amrrady <amrrady1@gmail.com>
2020-07-22 19:15:41 +0200

    add solution of change_db problem

--- C1_AlgorithmicToolbox/change_dp.py
+++ C1_AlgorithmicToolbox/change_dp.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
+
 import sys
 
-def get_change(m):
-    #write your code here
-    return m // 4
+def get_change(money, coins):
+    numCoins = {0:0}
+    for m in range(1, money + 1):
+        numCoins[ m ] = sys.maxsize
+        for coin in coins:
+            if m >= coin:
+                num = numCoins[m - coin] + 1
+                if num < numCoins[ m ]:
+                    numCoins[ m ] = num
+
+    return numCoins[ money ]
+
+def main():
+    n = int(input())
+    coins = [1, 3, 4]
+    print(get_change(n, coins))
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+    main()
