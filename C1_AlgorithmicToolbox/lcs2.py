5636c561710b09e6c4f3ff1073db05292a277edf
amrrady <amrrady1@gmail.com>
2020-07-26 20:33:32 +0200

    solution for 'Longest Common Subsequence of Two Sequences' problem

--- C1_AlgorithmicToolbox/lcs2.py
+++ C1_AlgorithmicToolbox/lcs2.py
@@ -2,20 +2,27 @@
 import sys
 
 def lcs2(a, b):
-    #write your code here
-    return min(len(a), len(b))
+    m, n = len(a) + 1, len(b) + 1
+    D =  [[0] * m for _ in range(n)] # N x M array of zeros
+
+    for j in range(1, m):
+        for i in range(1, n):
+            insertion = D[i][j-1]
+            deletion  = D[i-1][j]
+            mismatch  = D[i-1][j-1]
+            match  = D[i-1][j-1] + 1
+
+            if a[j-1] == b[i-1]:
+                D[i][j] = max(insertion, deletion, match)
+            else:
+                D[i][j] = max(insertion, deletion, mismatch)
+
+    return D[n-1][m-1]
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-
-    n = data[0]
-    data = data[1:]
-    a = data[:n]
-
-    data = data[n:]
-    m = data[0]
-    data = data[1:]
-    b = data[:m]
+    n = int(input())
+    a = list(map(int, input().split()))
+    m = int(input())
+    b = list(map(int, input().split()))
 
     print(lcs2(a, b))
