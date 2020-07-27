5c88ecd6597cc9d9e1e217f647c3dea440b4baa7
amrrady <amrrady1@gmail.com>
2020-07-27 11:54:17 +0200

    solution for 'Longest Common Subsequence of THREE Sequences' problem

--- C1_AlgorithmicToolbox/lcs3.py
+++ C1_AlgorithmicToolbox/lcs3.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python3
 import sys
 
+def dim(a):
+    if not type(a) == list:
+        return []
+    return [len(a)] + dim(a[0])
+
 def lcs3(a, b, c):
-    #write your code here
-    return min(len(a), len(b), len(c))
+    x, y, z = len(a), len(b), len(c)
+    D =  [[[0 for i in range(z+1)] for j in range(y+1)] for k in range(x+1)] 
+
+    for i in range(1, x+1):
+        for j in range(1, y+1):
+            for k in range(1, z+1):
+                if (a[i-1] == b[j-1] and a[i-1] == c[k-1]): 
+                    D[i][j][k] = D[i-1][j-1][k-1] + 1
+                else:
+                    D[i][j][k] = max(D[i-1][j][k], D[i][j-1][k], D[i][j][k-1])
+
+
+    return D[x][y][z]
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    an = data[0]
-    data = data[1:]
-    a = data[:an]
-    data = data[an:]
-    bn = data[0]
-    data = data[1:]
-    b = data[:bn]
-    data = data[bn:]
-    cn = data[0]
-    data = data[1:]
-    c = data[:cn]
+    _ = int(input())
+    a = list(map(int, input().split()))
+    _ = int(input())
+    b = list(map(int, input().split()))
+    _ = int(input())
+    c = list(map(int, input().split()))
     print(lcs3(a, b, c))
