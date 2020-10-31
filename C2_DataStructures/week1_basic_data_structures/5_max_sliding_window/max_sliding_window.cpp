a17b8e787c838b44513420949729af8ddcbf7da9
amrrady <amrrady1@gmail.com>
2020-10-31 21:10:14 +0100

    add solution for max sliding window problem

--- C2_DataStructures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.cpp
+++ C2_DataStructures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 
 using std::cin;
 using std::cout;
 using std::vector;
+using std::deque;
 using std::max;
 
 void max_sliding_window_naive(vector<int> const & A, int w) {
-    for (size_t i = 0; i < A.size() - w + 1; ++i) {
-        int window_max = A.at(i);
-        for (size_t j = i + 1; j < i + w; ++j)
-            window_max = max(window_max, A.at(j));
+    deque<int> Qi(w); // deque for indexes
 
-        cout << window_max << " ";
+    // first get candidate elements to be max from the first w elements
+    // do this by remove any early element less than of equal the current element
+
+    for (size_t i = 0; i < w; i++) {
+        while (!Qi.empty() && A[i] >= A[Qi.back()])
+            Qi.pop_back();
+
+        Qi.push_back(i);
     }
+
+    // now we have the max of the first w elements at the top of the Qi
+    // we print it, replace any element out of range (<= (i - w)), then repeat the above procedure
+
+    for (size_t i = w; i < A.size(); i++) {
+        cout << A[Qi.front()] << " ";
+
+        while (!Qi.empty() && Qi.front() <= (i - w))
+            Qi.pop_front();
+
+        while (!Qi.empty() && A[i] >= A[Qi.back()])
+            Qi.pop_back();
+
+        Qi.push_back(i);
+    }
+
+    // NOW we need to print the last Max
+    cout << A[Qi.front()];
 
     return;
 }
