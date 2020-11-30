20962d326fcdc460d24676498edf7d587dbb884e
amrrady <amrrady1@gmail.com>
2020-11-30 13:56:48 +0200

    add solution for "Convert array into heap" problem

--- C2_DataStructures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.cpp
+++ C2_DataStructures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.cpp
@@ -10,54 +10,75 @@ using std::pair;
 using std::make_pair;
 
 class HeapBuilder {
- private:
-  vector<int> data_;
-  vector< pair<int, int> > swaps_;
+private:
+    vector<int> data_;
+    vector< pair<int, int> > swaps_;
 
-  void WriteResponse() const {
-    cout << swaps_.size() << "\n";
-    for (int i = 0; i < swaps_.size(); ++i) {
-      cout << swaps_[i].first << " " << swaps_[i].second << "\n";
-    }
-  }
-
-  void ReadData() {
-    int n;
-    cin >> n;
-    data_.resize(n);
-    for(int i = 0; i < n; ++i)
-      cin >> data_[i];
-  }
-
-  void GenerateSwaps() {
-    swaps_.clear();
-    // The following naive implementation just sorts 
-    // the given sequence using selection sort algorithm
-    // and saves the resulting sequence of swaps.
-    // This turns the given array into a heap, 
-    // but in the worst case gives a quadratic number of swaps.
-    //
-    // TODO: replace by a more efficient implementation
-    for (int i = 0; i < data_.size(); ++i)
-      for (int j = i + 1; j < data_.size(); ++j) {
-        if (data_[i] > data_[j]) {
-          swap(data_[i], data_[j]);
-          swaps_.push_back(make_pair(i, j));
+    void WriteResponse() const {
+        cout << swaps_.size() << "\n";
+        for (int i = 0; i < swaps_.size(); ++i) {
+            cout << swaps_[i].first << " " << swaps_[i].second << "\n";
         }
-      }
-  }
+    }
 
- public:
-  void Solve() {
-    ReadData();
-    GenerateSwaps();
-    WriteResponse();
-  }
+    void ReadData() {
+        int n;
+        cin >> n;
+        data_.resize(n);
+        for(int i = 0; i < n; ++i)
+            cin >> data_[i];
+    }
+
+    int getParent(int i){
+        if(i > 0)
+            return (i-1)/2;
+        return 0;
+    }
+
+    int getLeftChild(int i){
+        return (2*i + 1);
+    }
+
+    int getRightChild(int i){
+        return (2*i + 2);
+    }
+
+    void SiftDown(int i){
+        int maxIndex = i;
+        int left  = getLeftChild(i);
+        int right = getRightChild(i);
+
+        if(left < this->data_.size() && this->data_[left] < this->data_[maxIndex])
+            maxIndex = left;
+
+        if(right < this->data_.size() && this->data_[right] < this->data_[maxIndex])
+            maxIndex = right;
+
+        if(maxIndex != i){
+            swap(data_[i], data_[maxIndex]);
+            swaps_.push_back(make_pair(i, maxIndex));
+            SiftDown(maxIndex);
+        }
+    }
+
+    void GenerateSwaps() {
+        swaps_.clear();
+        for (int i = data_.size()/2; i >= 0; i--)
+            SiftDown(i);
+    }
+
+
+public:
+    void Solve() {
+        ReadData();
+        GenerateSwaps();
+        WriteResponse();
+    }
 };
 
 int main() {
-  std::ios_base::sync_with_stdio(false);
-  HeapBuilder heap_builder;
-  heap_builder.Solve();
-  return 0;
+    std::ios_base::sync_with_stdio(false);
+    HeapBuilder heap_builder;
+    heap_builder.Solve();
+    return 0;
 }
